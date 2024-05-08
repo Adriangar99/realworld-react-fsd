7c940363c2c88d04cc68f5d8b134249c922409a4
Adrián García <adriangargar99@gmail.com>
2024-05-08 20:13:07 +0200

    chore: delete eslint

--- src/e2e-tests/article.spec.ts
+++ src/e2e-tests/article.spec.ts
@@ -1,37 +1,37 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { test, expect } from '@playwright/test';
+import { test, expect } from './fixture';
 import {
-  ARTICLE_RESUME,
-  ARTICLE_TEXT,
-  ARTICLE_TITLE,
+    ARTICLE_RESUME,
+    ARTICLE_TEXT,
+    ARTICLE_TITLE,
 } from './fixtures/article';
 
 test('write an article', async ({ page }) => {
-  await page
-    .getByRole('link', {
-      name: /new article/i,
-    })
-    .click();
+    await page.goto('/');
+    await page
+        .getByRole('link', {
+            name: /new article/i,
+        })
+        .click();
 
-  await page.getByPlaceholder(/title/i).fill(ARTICLE_TITLE);
+    await page.getByPlaceholder(/title/i).fill(ARTICLE_TITLE);
 
-  await page
-    .getByPlaceholder("What's this article about?")
-    .fill(ARTICLE_RESUME);
+    await page
+        .getByPlaceholder("What's this article about?")
+        .fill(ARTICLE_RESUME);
 
-  await page
-    .getByPlaceholder('Write your article (in markdown)')
-    .fill(ARTICLE_TEXT);
+    await page
+        .getByPlaceholder('Write your article (in markdown)')
+        .fill(ARTICLE_TEXT);
 
-  await page.getByPlaceholder(/tags/).fill('cats tdd');
+    await page.getByPlaceholder(/tags/).fill('cats tdd');
 
-  await page
-    .getByRole('button', {
-      name: /publish/i,
-    })
-    .click();
+    await page
+        .getByRole('button', {
+            name: /publish/i,
+        })
+        .click();
 
-  await expect(
-    page.getByRole('heading', { name: ARTICLE_TITLE }),
-  ).toBeVisible();
+    await expect(
+        page.getByRole('heading', { name: ARTICLE_TITLE }),
+    ).toBeVisible();
 });
