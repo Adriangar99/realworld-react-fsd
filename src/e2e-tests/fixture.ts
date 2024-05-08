6221f45fbbec6ae1c0a0c376c5df804645b6b73e
Adrián García <adriangargar99@gmail.com>
2024-05-08 20:26:25 +0200

    chore: ignore auth

--- src/e2e-tests/fixture.ts
+++ src/e2e-tests/fixture.ts
@@ -1,16 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { test as base, Page } from '@playwright/test';
-import { User, userForIndex } from './fixtures/users';
+import {test as base, Page} from '@playwright/test';
+import {User, userForIndex} from './fixtures/users';
 
 export * from '@playwright/test';
 
 // eslint-disable-next-line no-use-before-define
 const getParallelIndex = () => test.info().parallelIndex;
 
-const loginOrSignUp = async ({ page, user }: { page: Page; user: User }) => {
+const loginOrSignUp = async ({page, user}: { page: Page; user: User }) => {
     try {
         await page.goto('/');
-        await page.getByText('Your Feed').waitFor({ timeout: 300 });
+        await page.getByText('Your Feed').waitFor({timeout: 300});
     } catch {
         await page.goto('/login');
         await page.getByPlaceholder(/email/i).fill(user.email);
@@ -21,7 +21,7 @@ const loginOrSignUp = async ({ page, user }: { page: Page; user: User }) => {
             })
             .click();
         try {
-            await page.waitForURL(/profile/i, { timeout: 300 });
+            await page.waitForURL(/profile/i, {timeout: 300});
         } catch (error) {
             await page.goto('/register');
             await page.getByPlaceholder(/name/i).fill(user.name);
@@ -40,22 +40,22 @@ export const test = base.extend<
     { user: User },
     { workerStorageState: string }
 >({
-    storageState: ({ workerStorageState }, use) => use(workerStorageState),
-    user: async ({ page: _ }, use) => {
+    storageState: ({workerStorageState}, use) => use(workerStorageState),
+    user: async ({page: _}, use) => {
         const id = getParallelIndex();
         const user = userForIndex(id);
         await use(user);
     },
     workerStorageState: [
-        async ({ browser }, use) => {
+        async ({browser}, use) => {
             const id = getParallelIndex();
             const user = userForIndex(id);
             const app = await user.instance(browser, 'http://localhost:5173');
-            await loginOrSignUp({ page: app.page, user });
-            await app.page.context().storageState({ path: user.path });
+            await loginOrSignUp({page: app.page, user});
+            await app.page.context().storageState({path: user.path});
             await use(user.path);
             await app.context.close();
         },
-        { scope: 'worker' },
+        {scope: 'worker'},
     ],
 });
